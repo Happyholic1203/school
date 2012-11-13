95d76c3eee6d77ce4503db4e6d9cb012c6454109
Happyholic1203 <Happyholic1203@gmail.com>
2012-11-13 18:07:38 +0800

    Created data mining in 101-1

---
+++ 101-1/data_mining/hw4/classify.cpp
@@ -0,0 +1,10 @@
+/****************************************************************************
+FileName     [ classify.cpp ]
+Synopsis     [ data mining hw#4]
+Author       [ Yu-Cheng (Henry) Huang ]
+Copyright    [ Copyleft(c) 2012 NTUEE, Taiwan ]
+****************************************************************************/
+
+int main(){
+	return 0;
+}
