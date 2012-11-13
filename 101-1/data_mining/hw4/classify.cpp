490e0857d548ca0b6f37f5d19af87a7ae71263b5
Happyholic1203 <Happyholic1203@gmail.com>
2012-11-13 18:09:16 +0800

    test

--- 101-1/data_mining/hw4/classify.cpp
+++ 101-1/data_mining/hw4/classify.cpp
@@ -5,6 +5,9 @@ Author       [ Yu-Cheng (Henry) Huang ]
 Copyright    [ Copyleft(c) 2012 NTUEE, Taiwan ]
 ****************************************************************************/
 
+#include <iostream>
+using namespace std;
+
 int main(){
 	return 0;
 }
